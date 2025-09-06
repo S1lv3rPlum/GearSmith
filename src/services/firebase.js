959dd3bf128e654b493662a16071f95c3a09b904
S1lv3rPlum <Reneeslueke@gmail.com>
2025-09-06 11:46:20 -0400

    Update firebase.js
    fix missing quotes, change the storage bucket field

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,8 +5,8 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDc9E-DaEBzabNCqFuMyQ9aRcxmCMDHHgE",
   authDomain: "gearsmith-e3302.firebaseapp.com",
-  projectId: gearsmith-e3302,
-  storageBucket: gearsmith-e3302.firebasestorage.app,
+  projectId: "gearsmith-e3302",
+  storageBucket: "gearsmith-e3302.appspot.com",
   messagingSenderId: "950900631942",
   appId: "1:950900631942:android:d62b1cd381"
 };
